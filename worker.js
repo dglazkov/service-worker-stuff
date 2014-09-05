72116cdb92b483a7c236ef930a18967141022f1d
Dominic Cooney <dominicc@chromium.org>
2014-09-06 08:25:07 +0900

    Try to read and write caches.

--- worker.js
+++ worker.js
@@ -1,4 +1,8 @@
-self.debug = {version: 3};
+// Debugging tips:
+// - Bump the self.debug version line to sanity check the script in Inspector.
+// - Use polyfillCaches.delete('stuff') to trash the cache.
+
+self.debug = {version: 8};
 
 // FIXME: Make this order-insensitive by using ES6 modules when they
 // are available.
@@ -9,7 +13,15 @@ importScripts(
     'bower_components/cache-polyfill/caches.js');
 
 // Defines which paths are of interest and will be cached.
-var cachedResourcePath = /^\/$|^\/index\.html$|^\/bower_components\//;
+var cachedResourcePath =
+    /^\/$|^\/index\.html$|^\/bower_components\/|^\/elements\//;
+
+self.oninstall = function(event) {
+  // Ignore failures from 'create', which probably means the cache
+  // already exists.
+  event.waitUntil(polyfillCaches.create('stuff')
+    .then(function(){}, function(){}));
+};
 
 self.onfetch = function(event) {
   self.debug.lastEvent = event;
@@ -19,5 +31,24 @@ self.onfetch = function(event) {
     return;  // This isn't cached; use the network.
   }
 
-  // Use the network anyway for now.
+  var cache;
+
+  console.log(event.request.url);
+
+  event.respondWith(
+    polyfillCaches.get('stuff')
+      .then(function(c) {
+          cache = c;
+          return cache.match(event.request);
+        })
+      .then(function(response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request.url)
+            .then(function(response) {
+                cache.put(event.request, response);
+                return response;
+              });
+        }));
 };
